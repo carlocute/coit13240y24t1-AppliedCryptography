043ba9452e14d0df787f5669d5f908fc59900236
carlocute <144578050+carlocute@users.noreply.github.com>
2024-05-28 18:38:15 +1000

    Update diffie_hellman.py

--- diffie_hellman.py
+++ diffie_hellman.py
@@ -1,1 +1,24 @@
 
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+
+def generate_dh_keypair():
+    private_key = ec.generate_private_key(ec.SECP256R1())
+    public_key = private_key.public_key().public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    return private_key, public_key
+
+def generate_shared_secret(private_key, peer_public_key):
+    peer_public_key = serialization.load_pem_public_key(peer_public_key)
+    shared_key = private_key.exchange(ec.ECDH(), peer_public_key)
+    return HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=None,
+        backend=None
+    ).derive(shared_key)
